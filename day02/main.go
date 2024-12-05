98ada2011b3677b7dda435d0790a1cb76df294ca
nobilissimum <ronn.angelo.lee@gmail.com>
2024-12-06 02:30:50 +0800

    Finish day 2 part 2

--- day02/main.go
+++ day02/main.go
@@ -10,6 +10,34 @@ import (
 
 const inputFile = "input"
 
+func safeCheck(lineSlice []string) bool {
+	direction := 1
+	isSafe := true
+	for x := 1; x < len(lineSlice); x++ {
+		num, err := strconv.Atoi(lineSlice[x])
+		if err != nil {
+			log.Panicf("%v contains non-numeric data %v\n", lineSlice, lineSlice[x])
+		}
+
+		numPrev, err := strconv.Atoi(lineSlice[x-1])
+		if err != nil {
+			log.Panicf("%v contains non-numeric data %v\n", lineSlice, lineSlice[x-1])
+		}
+
+		difference := numPrev - num
+		if x == 1 && difference < 0 {
+			direction = -1
+		}
+
+		if difference*direction < 1 || difference*direction > 3 {
+			isSafe = false
+			break
+		}
+	}
+
+	return isSafe
+}
+
 func main() {
 	content, err := os.ReadFile(inputFile)
 	if err != nil {
@@ -18,41 +46,24 @@ func main() {
 
 	lines := strings.Split(string(content), "\n")
 	safeReportQuantity := 0
-	for y, _line := range lines {
+	for _, _line := range lines {
 		if _line == "" {
 			continue
 		}
 
 		line := strings.Split(_line, " ")
-		isSafe := true
-		direction := 1
-		for x := 1; x < len(line); x++ {
-			num, err := strconv.Atoi(line[x])
-			if err != nil {
-				log.Panicf("Line %v (%v) contains non-numeric data %v\n", y, line, line[x])
-			}
+		isSafe := false
+		for x := 0; x < len(line); x++ {
+			lineSlice := make([]string, 0, len(line)-1)
+			lineSlice = append(lineSlice, line[:x]...)
+			lineSlice = append(lineSlice, line[x+1:]...)
 
-			numPrev, err := strconv.Atoi(line[x-1])
-			if err != nil {
-				log.Panicf("Line %v (%v) contains non-numeric data %v\n", y, line, line[x-1])
-			}
-
-			difference := numPrev - num
-			if x == 1 && difference < 0 {
-				direction = -1
-			}
-
-			if difference*direction < 1 || difference*direction > 3 {
-				isSafe = false
+			isSafe = safeCheck(lineSlice)
+			if isSafe {
+				safeReportQuantity += 1
 				break
 			}
 		}
-
-		if !isSafe {
-			continue
-		}
-
-		safeReportQuantity += 1
 	}
 
 	fmt.Printf("The number of reports that are safe is: %v\n", safeReportQuantity)
